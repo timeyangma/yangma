614ae451d89ca285c8c8d5106bf2b65e1a1595cb
unknown <timeyangma@gmail.com>
2020-01-09 10:44:15 +0900

    테스트 커밋

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ import os
 strA = "hellow"
 print(strA)
 
-f = open("E:/새파일.txt",'a')
+f = open("E:/파이썬새파일입니다..txt",'a')
 for i in range(11, 20):
     data = "%d번째 줄입니다.\n" % i
     f.write(data)
 f.write(strA)
 f.close()
+
 
 
